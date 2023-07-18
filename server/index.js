31b9367644dc4843953c18568fa3abdc88854bd4
Rugved Wagh <89897353+Rugved76@users.noreply.github.com>
2023-07-18 13:37:43 +0530

    Update index.js changed the origin cors

--- server/index.js
+++ server/index.js
@@ -17,8 +17,6 @@ const allowedOrigins = [
 // Use the CORS middleware with allowed origin links
 app.use(cors({
   origin: allowedOrigins,
-  methods: 'GET, POST, PUT, DELETE',
-  allowedHeaders: 'Content-Type, Authorization',
   credentials: true,
 }));
 
