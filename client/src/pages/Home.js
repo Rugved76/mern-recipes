42440f669b967720ac74ca3472ce35a8c5e59628
Rugved Wagh <89897353+Rugved76@users.noreply.github.com>
2023-07-19 01:21:55 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,7 +4,7 @@ import { useGetUserID } from "../hooks/useGetUserID";
 import axios from "axios";
 import { Link, useParams } from "react-router-dom";
 // export const url = `http://localhost:3001`
-export const url = `https://recipebackend-x1g8.onrender.com`
+export const url = `https://recipeserver-odjx.onrender.com`
 
 
 export const Home = () => {
