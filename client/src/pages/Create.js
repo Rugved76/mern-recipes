ffbdfbfe19a101a87446fbce9dab8de201cf76b9
Rugved Wagh <89897353+Rugved76@users.noreply.github.com>
2023-07-19 15:50:04 +0530

    Update Create.js

--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -6,60 +6,60 @@ import { useCookies } from "react-cookie";
 import { url } from "./Home";
 
 export const CreateRecipe = () => {
-  const userID = useGetUserID();
-  const [cookies, _] = useCookies(["access_token"]);
+    const userID = useGetUserID();
+    const [cookies, _] = useCookies(["access_token"]);
 
-  const [recipe, setRecipe] = useState({
-    name: "",
-    instructions: "",
-    userOwner: userID,
-  });
+    const [recipe, setRecipe] = useState({
+        name: "",
+        instructions: "",
+        userOwner: userID,
+    });
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setRecipe({ ...recipe, [name]: value });
-  };
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setRecipe({ ...recipe, [name]: value });
+    };
 
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      await axios.post(`${url}/recipes`, { ...recipe },
-        {
-          headers: { authorization: cookies.access_token },
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post(`${url}/recipes`, { ...recipe },
+                {
+                    headers: { authorization: cookies.access_token },
+                }
+            );
+
+            navigate("/");
+        } catch (error) {
+            console.error(error);
         }
-      );
+    };
 
-      navigate("/");
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    return (
+        <div className="create-recipe">
+            <h2 className="foh2">Add </h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Title</label>
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={recipe.name}
+                    onChange={handleChange}
+                />
 
-  return (
-    <div className="create-recipe">
-      <h2 className="foh2">Add </h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Title</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={recipe.name}
-          onChange={handleChange}
-        />
-
-        <label htmlFor="instructions">Description</label>
-        <textarea
-          id="instructions"
-          name="instructions"
-          value={recipe.instructions}
-          onChange={handleChange}
-        ></textarea>
-        <button className='submit'type="submit">Create </button>
-      </form>
-    </div>
-  );
+                <label htmlFor="instructions">Description</label>
+                <textarea
+                    id="instructions"
+                    name="instructions"
+                    value={recipe.instructions}
+                    onChange={handleChange}
+                ></textarea>
+                <button className='submit' type="submit">Create </button>
+            </form>
+        </div>
+    );
 };
