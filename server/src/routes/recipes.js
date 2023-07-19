cd531240e1109ec0720d6f8ee0a8c9b44631418b
Rugved Wagh <89897353+Rugved76@users.noreply.github.com>
2023-07-19 18:41:08 +0530

    Update recipes.js

--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -6,113 +6,113 @@ import { verifyToken } from "./user.js";
 
 const router = express.Router();
 
+// Landing page
 router.get("/", async (req, res) => {
-  try {
-    const result = await RecipesModel.find({});
-    res.status(200).json(result);
-    console.log('\nHome page loaded with posts...')
-  } catch (err) {
-    res.status(500).json(err);
-  }
+    try {
+        const post = await RecipesModel.find();
+        res.status(200).json(post);
+        console.log('\nPosts fetched...')
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 // Create a new recipe
 router.post("/", verifyToken, async (req, res) => {
-  const recipe = new RecipesModel({
-    _id: new mongoose.Types.ObjectId(),
-    name: req.body.name,
-    instructions: req.body.instructions,
-    userOwner: req.body.userOwner,
-  });
-  console.log(recipe);
-
-  try {
-    const result = await recipe.save();
-    res.status(201).json({
-      createdRecipe: {
-        name: result.name,
-        instructions: result.instructions,
-        _id: result._id,
-      },
+    const recipe = new RecipesModel({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        instructions: req.body.instructions,
+        userOwner: req.body.userOwner,
     });
-    console.log('\nCreated post successfully...')
-  } catch (err) {
-    res.status(500).json(err);
-  }
+    console.log(recipe);
+
+    try {
+        const result = await recipe.save();
+        res.status(201).json({
+            createdRecipe: {
+                name: result.name,
+                instructions: result.instructions,
+                _id: result._id,
+            },
+        });
+        console.log('\nPost created...')
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 // Save a Recipe
 router.put("/", async (req, res) => {
-  const recipe = await RecipesModel.findById(req.body.recipeID);
-  const user = await UserModel.findById(req.body.userID);
-  try {
-    user.savedRecipes.push(recipe);
-    await user.save();
-    res.status(201).json({ savedRecipes: user.savedRecipes });
-    console.log('\nPost saved successfully...')
-  } catch (err) {
-    res.status(500).json(err);
-  }
+    const recipe = await RecipesModel.findById(req.body.recipeID);
+    const user = await UserModel.findById(req.body.userID);
+    try {
+        user.savedRecipes.push(recipe);
+        await user.save();
+        res.status(201).json({ savedRecipes: user.savedRecipes });
+        console.log('\nPost saved...')
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 // Get id of saved recipes
 router.get("/savedRecipes/ids/:userId", async (req, res) => {
-  try {
-    const user = await UserModel.findById(req.params.userId);
-    res.status(201).json({ savedRecipes: user?.savedRecipes });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+    try {
+        const user = await UserModel.findById(req.params.userId);
+        res.status(201).json({ savedRecipes: user?.savedRecipes });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
 
 // Get saved recipes
 router.get("/savedRecipes/:userId", async (req, res) => {
-  try {
-    const user = await UserModel.findById(req.params.userId);
-    const savedRecipes = await RecipesModel.find({
-      _id: { $in: user.savedRecipes },
-    });
+    try {
+        const user = await UserModel.findById(req.params.userId);
+        const savedRecipes = await RecipesModel.find({
+            _id: { $in: user.savedRecipes },
+        });
 
-    console.log('\nFetched saved posts successfully...');
-    res.status(201).json({ savedRecipes });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+        console.log('\nSaved post fetched...');
+        res.status(201).json({ savedRecipes });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
 
 // Single post page
 
 router.get('/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const recipe = await RecipesModel.findById(id)
-    console.log('\n\nFetched successfully...\n')
-    console.log(recipe)
-    res.status(200).json(recipe);
-  } catch (err) {
-    console.log(err)
-    res.status(500).json(err)
-  }
+    try {
+        const { id } = req.params;
+        const recipe = await RecipesModel.findById(id)
+        console.log('\nPost fetched...\n')
+        console.log(recipe)
+        res.status(200).json(recipe);
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
 })
 
 router.delete('/:id', async (req, res) => {
-  try {
-    const recipe = await RecipesModel.findByIdAndDelete({_id:req.params});
+    try {
+        const recipe = await RecipesModel.findByIdAndDelete({ _id: req.params });
 
-    if (!recipe) {
-      return res.status(404).json({ message: 'Recipe not found' });
+        if (!recipe) {
+            return res.status(404).json({ message: 'Recipe not found' });
+        }
+
+        console.log('\n\nPost deleted...\n');
+        console.log(recipe);
+        res.status(200).json({ message: 'Recipe deleted successfully' });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: err.message });
     }
-
-    console.log('\n\nDeleted post successfully...\n');
-    console.log(recipe);
-
-    res.status(200).json({ message: 'Recipe deleted successfully' });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: err.message });
-  }
 });
 
 
