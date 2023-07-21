f22effe8ac4e4efbe78bcf865761d17c334ac6ea
Rugved Wagh <89897353+Rugved76@users.noreply.github.com>
2023-07-21 10:28:58 +0530

    Add files via upload

---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+	<div className='footer'>
+		<ul>
+			<li>Created by : Rugved</li>
+			<li><a  style={{textDecoration:'none'}} href='https://github.com/Rugved76/mern-recipes.git'>Github</a></li>
+			<li><a>LinkedIn</a></li>
+		</ul> 
+	</div>
+  )
+}
